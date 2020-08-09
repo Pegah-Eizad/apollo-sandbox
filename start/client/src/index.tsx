064d873a05f2d466530d525c366842f469050625
Pegah Eizad <peizadkh@gmail.com>
2020-08-09 11:35:22 -0400

    connects api to react hooks client

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,9 +1,18 @@
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
+import {
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client'
 import { HttpLink } from 'apollo-link-http';
+import gql from 'graphql-tag';
+import { ApolloProvider } from '@apollo/react-hooks';
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+import Pages from './pages';
+import injectStyles from './styles'
 
 const cache = new InMemoryCache();
-const link = new HttpLink({
+const link: any = new HttpLink({
   uri: 'http://localhost:4000/'
 });
 
@@ -11,3 +20,11 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   link
 });
+
+injectStyles();
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>, 
+  document.getElementById('root')
+);
